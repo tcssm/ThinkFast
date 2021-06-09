b120edd88f34945c6c8fd27df4357d59140b8a67
Deanna Yee <deanna.a.yee@gmail.com>
2021-06-08 17:10:48 -0700

    Initial Commit

---
+++ ThinkFast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThinkFast
+//
+//  Created by Deanna Yee on 6/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
